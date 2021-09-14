a431c45890fa416caa91c885241bb62fde08c241
vorwinator <vorwi111@gmail.com>
2021-09-14 18:31:00 +0200

    stawiany projekt

---
+++ application/controllers/Account.php
@@ -0,0 +1,12 @@
+<?php
+    class Account extends MY_Controller{
+        public function __construct()
+        {
+            parent::__construct();
+        }
+        public function index(){
+            $data['welcome'] = "To jest domek";
+            
+            $this->view("pages/admin/", "index", $data);
+        }
+    }
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,21 @@
+<?php
+    class MY_Controller extends CI_Controller{
+        public function construct()
+        {
+            parent::__construct();
+        }
+        public function view($page_path, $page, $data){
+            $full_path=$page_path.$page;
+            if ( ! file_exists('./application/views/'.$full_path.'.php'))
+            {
+                    // Whoops, we don't have a page for that!
+                    show_404();
+            }
+
+            $data['title'] = ucfirst($page); // Capitalize the first letter
+
+            $this->load->view('templates/header', $data);
+            $this->load->view($full_path, $data);
+            $this->load->view('templates/footer', $data);
+        }
+    }
