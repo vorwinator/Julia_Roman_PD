d787d305e32db09587e519b67d4330a4ae8b4979
vorwinator <vorwi111@gmail.com>
2021-09-21 14:07:37 +0200

    zmiana ścieżki

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -7,6 +7,6 @@
         public function index(){
             $data['welcome'] = "To jest domek";
             
-            $this->render_page("pages/admin/", "index", $data);
+            $this->render_page("pages/", "index", $data);
         }
     }
