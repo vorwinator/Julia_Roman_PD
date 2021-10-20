18ac3b1bf6b6fdb4e34669f3a8faa727ec82f26a
vorwinator <vorwi111@gmail.com>
2021-10-20 20:58:06 +0200

    dodanie kontroli logowania

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -3,13 +3,13 @@
         public function construct()
         {
             parent::__construct();
-            $this->load->model('authentication');
+            $this->load->model('authentication_m');
             $this->load->library(array('session','form_validation'));
             $this->load->helper(array('url','form'));
         }
         public function login(){
             if(isset($_SESSION['id_acc_login'])){
-                redirect('client');
+                redirect('main/index');
             }
             else{
                 $this->form_validation->set_rules('password', 'Hasło', 'required', array('required' => 'Musisz wpisać %s.'));
@@ -17,21 +17,28 @@
     
                 if ($this->form_validation->run() === FALSE)
                     {
-                        $this->load->view('login');
+                        $this->load->view('templates/header');
+                        $this->load->view('pages/authentication/login');
+                        $this->load->view('templates/footer');
                     }
                     else
                     {
-                        $pass=$this->authentication->check_password($this->input->post('email'));
+                        $this->load->model('authentication_m');
+                        $pass=$this
+                        ->authentication_m
+                        ->check_password($this->input->post('email'));
     
-                        if($pass!=md5($this->input->post('password'))){
+                        if($pass->password!=md5($this->input->post('password'))){
                             $data['info']="Podano nieprawidłowe dane logowania";
-                            $this->load->view('login',$data);
+                            $this->load->view('templates/header');
+                            $this->load->view('pages/authentication/login',$data);
+                            $this->load->view('templates/footer');
                         }
                         else{
-                            $account=$this->authentication->get_acc_by_email($this->input->post('email'));
+                            $account=$this->authentication_m->get_acc_by_email($this->input->post('email'));
 
                             $this->load->view('templates/header');
-                            $this->load->view('client/mainpage');
+                            $this->load->view('pages/main/index');
                             $this->load->view('templates/footer');
                         }
                         
