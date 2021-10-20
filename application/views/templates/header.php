0aae5debb6a23f1293693e209ade9328c3d12cce
vorwinator <vorwi111@gmail.com>
2021-10-20 21:07:01 +0200

    linki nawigacyjne

---
+++ application/views/templates/header.php
@@ -0,0 +1,4 @@
+<a href=<?php echo site_url('account');?>>Konto</a> </br>
+<a href=<?php echo site_url('account/create');?>>Strona</a> </br>
+<a href=<?php echo site_url('authentication/login');?>>Logowanie</a> </br>
+<a href=<?php echo site_url('main/index');?>>Główna</a> </br>
