d5f845f95911f0a9e541398751e0b0657d76d937
vorwinator <vorwi111@gmail.com>
2021-10-22 21:26:31 +0200

    wylogowanie, poprawki przypisywania danych zalogowanego, zmiana ścieżek

--- application/controllers/Authentication.php
+++ application/controllers/Authentication.php
@@ -7,10 +7,24 @@
             $this->load->library(array('session','form_validation'));
             $this->load->helper(array('url','form'));
         }
+        public function logout()
+        {
+            unset($_SESSION['login_id_acc']);
+            unset($_SESSION['login_email']);
+            unset($_SESSION['login_firstname']);
+            unset($_SESSION['login_lastname']);
+            unset($_SESSION['login_acc_type']);
+
+            $data['info']="Pomyślnie wylogowano";
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('pages/authentication/login');//temp
+            $this->load->view('templates/footer');            
+        }
         public function login(){
             if(isset($_SESSION['login_id_acc'])){
                 if($_SESSION['login_acc_type']==1){
-                    redirect('main/index');//temp
+                    redirect('pages/admin/account/create');//temp
                 }
                 else{
                     redirect('main/index');//temp
@@ -22,7 +36,13 @@
     
                 if ($this->form_validation->run() === FALSE)
                     {
-                        $this->load->view('templates/header');
+                        if(isset($_GET['info'])){
+                            $data['info']=$_GET['info'];
+                            $this->load->view('templates/header', $data);
+                        }
+                        else{
+                            $this->load->view('templates/header');
+                        }
                         $this->load->view('pages/authentication/login');
                         $this->load->view('templates/footer');
                     }
@@ -33,28 +53,28 @@
     
                         if($pass->password!=md5($this->input->post('password'))){
                             $data['info']="Podano nieprawidłowe dane logowania";
-                            $this->load->view('templates/header');
-                            $this->load->view('pages/authentication/login',$data);
+                            $this->load->view('templates/header', $data);
+                            $this->load->view('pages/authentication/login');
                             $this->load->view('templates/footer');
                         }
                         else{
                             $account=$this->authentication_m->get_acc_by_email($this->input->post('email'));
 
-                            $_SESSION['login_id_acc']=$account['id_acc'];
-                            $_SESSION['login_email']=$account['email'];
-                            $_SESSION['login_firstname']=$account['firstname'];
-                            $_SESSION['login_lastname']=$account['lastname'];
-                            $_SESSION['login_acc_type']=$account['acc_type'];
+                            $_SESSION['login_id_acc']=$account->id_acc;
+                            $_SESSION['login_email']=$account->email;
+                            $_SESSION['login_firstname']=$account->firstname;
+                            $_SESSION['login_lastname']=$account->lastname;
+                            $_SESSION['login_acc_type']=$account->acc_type;
 
                             if($_SESSION['login_acc_type']==null){
-                                $data['info']="Wystąpił niespodziewany błąd.";
+                                $data['info']="Wystąpił niespodziewany błąd.";//źle
                                 redirect('main/index');//temp
                             }
 
                             if($_SESSION['login_acc_type']==1)
                             {
                                 $this->load->view('templates/header');
-                                $this->load->view('pages/main/index');//temp
+                                $this->load->view('pages/admin/account/create');//temp
                                 $this->load->view('templates/footer');
                             }
                             else{
