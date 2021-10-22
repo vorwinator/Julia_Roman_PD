2b4ca3ea577f1fb54eb6ec6ee2db79f2fb9cd316
vorwinator <vorwi111@gmail.com>
2021-10-22 21:29:30 +0200

    zabezpieczenia przed nieautoryzowanym dostępem do stron admina

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,8 +1,20 @@
 <?php
     class MY_Controller extends CI_Controller{
-        public function construct()
+        public function __construct()
         {
             parent::__construct();
+            if(str_contains(current_url(),'admin')){
+                if(!isset($_SESSION['login_acc_type'])){
+                    $data['info']="Musisz być zalogowany by mieć do tego dostęp.";
+                    redirect('authentication/login?info=Musisz być zalogowany by mieć do tego dostęp.');
+                }
+                if(isset($_SESSION['login_acc_type'])){
+                    if($_SESSION['login_acc_type']!=1){
+                        $data['info']="Musisz mieć prawa administratora aby uzyskać dostęp do tej witryny";
+                        redirect('authentication/login?info=Musisz mieć prawa administratora aby uzyskać dostęp do tej witryny');
+                    }
+                }
+            }
         }
         public function render_page($page_path, $page, $data){
             $full_path=$page_path.$page;
