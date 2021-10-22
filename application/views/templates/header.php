3b087b4e3a0dc2bb62dc18180782791f2acf8f78
vorwinator <vorwi111@gmail.com>
2021-10-22 21:31:07 +0200

    dodanie paru linków nawigacyjnych. dodanie okienka typu alert

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,4 +1,20 @@
-<a href=<?php echo site_url('account');?>>Konto</a> </br>
-<a href=<?php echo site_url('account/create');?>>Strona</a> </br>
+<!DOCTYPE html>
+<html>
+<head>
+    <script>
+        <?php if(isset($info)) { ?>
+            function infoWindow(message) {
+                alert(message);
+            }
+        <?php } ?>
+            function alertWindow() {
+                infoWindow("<?php if(isset($info)) echo $info ?>");
+            }
+    </script>
+</head>
+<body onload=alertWindow()>
+<a href=<?php echo site_url('admin/account');?>>Konto</a> </br>
+<a href=<?php echo site_url('admin/account/create');?>>Utwórz konto</a> </br>
 <a href=<?php echo site_url('authentication/login');?>>Logowanie</a> </br>
+<a href=<?php echo site_url('authentication/logout');?>>Wyloguj</a> </br>
 <a href=<?php echo site_url('main/index');?>>Główna</a> </br>
