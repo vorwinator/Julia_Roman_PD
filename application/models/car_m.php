be908a204246c4968e1b5c0aacf416b7c74a364e
vorwinator <vorwi111@gmail.com>
2021-11-01 20:37:42 +0100

    edycja i usuwanie danych samochodów

--- application/models/car_m.php
+++ application/models/car_m.php
@@ -60,4 +60,72 @@
 			$this->unit->run($respond, 'is_array', 'Czy dane o samochodach są dostarczane w formie tabeli danych.');
 			return $respond;
 		}
+
+
+		public function get_car_by_id($id)
+		{
+            $query = $this->db->select('*')
+			->from('car_details')
+			->join('car', 'car.id_car=car_details.id_car')
+			->where('car.id_car', $id)
+			->get();
+
+			if($query->num_rows() > 0){
+                $respond = $query->row_array();
+            }else{
+                $respond = FALSE;
+            }
+
+			return $respond;
+        }
+
+
+		public function update($id_car)
+		{
+            $data = array(
+				'release_year' => $this->input->post('release_year'),
+				'price_per_day' => $this->input->post('price_per_day'),
+				'mileage' => $this->input->post('mileage'),
+				'rental_status' => $this->input->post('rental_status'),
+				'insurance' => $this->input->post('insurance')
+			);
+			$this->update_details($id_car);
+
+            $this->db->set($data);
+
+            $this->db->where('id_car', $id_car);
+
+			$respond = $this->db->update('car');
+
+			$this->unit->run($respond, TRUE, 'Czy wysłano zmodyfikowane dane samochodu do bazy.');
+            return $respond;
+        }
+
+
+		public function update_details($id_car)
+		{
+            $data = array(
+                'brand' => $this->input->post('brand'),
+				'model' => $this->input->post('model'),
+				'average_consumption' => $this->input->post('average_consumption'),
+				'number_of_airbags' => $this->input->post('number_of_airbags'),
+				'number_of_doors' => $this->input->post('number_of_doors'),
+				'number_of_seats' => $this->input->post('number_of_seats'),
+				'drive' => $this->input->post('drive'),
+				'air_conditioning' => $this->input->post('air_conditioning'),
+				'gearbox' => $this->input->post('gearbox'),
+				'trunk_capacity' => $this->input->post('trunk_capacity'),
+				'deposit' => $this->input->post('deposit'),
+				'car_type' => $this->input->post('car_type')
+			);
+
+            $this->db->set($data);
+
+            $this->db->where('id_car', $id_car);
+
+			$respond = $this->db->update('car_details');
+
+			$this->unit->run($respond, TRUE, 'Czy wysłano zmodyfikowane dane szczegółów samochodu do bazy.');
+            return $respond;
+        }
     } 
