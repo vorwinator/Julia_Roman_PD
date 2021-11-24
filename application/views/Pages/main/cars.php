058af1e38033358d6e9cedc189d326a93803281a
vorwinator <vorwi111@gmail.com>
2021-11-24 22:31:42 +0100

    lista samochodów dla klienta niezalogowanego

---
+++ application/views/Pages/main/cars.php
@@ -0,0 +1,13 @@
+<?php
+    foreach ($cars as $car){
+        echo $car['brand']." | ";
+        echo $car['model']." | ";
+        echo $car['number_of_doors']." | ";
+        echo $car['number_of_seats']." | ";
+        echo $car['drive']." | ";
+        echo $car['fuel']." | ";
+        echo $car['air_conditioning']." | ";
+        echo $car['deposit']." | ";
+?>
+    <a href=<?php echo site_url('car_all/details?id_car_update='.$car['id_car']);?>>Wybierz</a></br>
+<?php } ?>
