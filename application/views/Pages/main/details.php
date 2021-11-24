f079594b6fe1aa62e3bfcfece1344cdc75690b30
vorwinator <vorwi111@gmail.com>
2021-11-24 23:47:53 +0100

    detale wynajmowanego auta

---
+++ application/views/Pages/main/details.php
@@ -0,0 +1,21 @@
+<?php
+        echo $car['release_year']." | ";
+        echo $car['fuel']." | ";
+        echo $car['price_per_day']." | ";
+        echo $car['mileage']." | ";
+        echo $car['rental_status']." | ";
+        echo $car['insurance']." | ";
+        echo $car['brand']." | ";
+        echo $car['model']." | ";
+        echo $car['average_consumption']." | ";
+        echo $car['number_of_airbags']." | ";
+        echo $car['number_of_doors']." | ";
+        echo $car['number_of_seats']." | ";
+        echo $car['drive']." | ";
+        echo $car['air_conditioning']." | ";
+        echo $car['gearbox']." | ";
+        echo $car['trunk_capacity']." | ";
+        echo $car['deposit']." | ";
+        echo $car['car_type']."</br>";
+?>
+        <a href=<?php echo site_url('admin/car/update?id_car_update='.$car['id_car']);?>>Wynajmij</a>
