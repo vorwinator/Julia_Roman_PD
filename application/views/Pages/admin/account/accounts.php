31fc8f1feef7e79fdd466c8cb1674354cd78cbff
vorwinator <vorwi111@gmail.com>
2021-12-16 23:38:45 +0100

    dodanie frontu(WYMAGA POPRAWEK)

--- application/views/Pages/admin/account/accounts.php
+++ application/views/Pages/admin/account/accounts.php
@@ -1,9 +1,69 @@
-<?php
-    foreach ($accounts as $account){
-        echo $account['id_acc']." | ";
-        echo $account['email']." | ";
-        echo $account['firstname']." | ";
-        echo $account['lastname']." | ";
-        echo $account['acc_type']."</br>";
-    }
-?>
+<div class="container-fluid">
+    <div class="col-md-8 col-lg-6 col-xl-4 offset-1 mx-auto">
+        <h1 class="big-header-style text-center">Lista kont</h1>
+    </div>
+    <div class="col-md-8 col-lg-12 text-center pd-bottom">
+        <a role="button" class="btn btn-outline-primary full-width-btn" href=<?php echo site_url('admin/account/create');?>>Stwórz konto</a>
+    </div>
+    <table id="example" class="display">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Email</th>
+                <th>Typ konta</th>
+                <th>Opcje</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($accounts as $account){
+            ?>
+            <tr>
+                <td>
+                    <?php
+                    echo $account['id_acc']
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    echo $account['firstname']
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    echo $account['lastname']
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    echo $account['email']
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    if($account['acc_type']==0) echo "Klient";
+                    elseif($account['acc_type']==1) echo "Admin";
+                    ?>
+                </td>
+                <td>
+                    <a href=<?php echo site_url('admin/account/create');?>><button type="button" class="btn btn-primary btn-custom">Edytuj</button></a>
+                </td>
+            </tr>
+            <?php
+            }
+            ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Id</th>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Email</th>
+                <th>Typ konta</th>
+                <th>Opcje</th>
+            </tr>
+        </tfoot>
+    </table>
+</div>
