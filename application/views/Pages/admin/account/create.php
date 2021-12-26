6745b79e36cf66fb87e568c2acc3bc78bfb9f5bc
vorwinator <80983036+vorwinator@users.noreply.github.com>
2021-12-26 14:37:21 +0100

    dodano placeholdery i kontenery na error

--- application/views/Pages/admin/account/create.php
+++ application/views/Pages/admin/account/create.php
@@ -1,20 +1,20 @@
 <div class="container-fluid d-flex justify-content-center">
     <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
         <form class="col-lg-9 pd-registr">
-        <?php
-        echo form_open(current_url(), 'class="col-lg-9 pd-registr"');
-        ?>
+            <?php
+            echo form_open(current_url(), 'class="col-lg-9 pd-registr"');
+            ?>
             <h1 class="big-header-style text-center">Dodaj konto</h1>
             <div class="pd-top">
                 <div class="form-floating mb-4">
                     <!-- <input type="imie" class="form-control form-control-lg" placeholder="imie" />
                     <label for="imie">Imię</label> -->
                     <?php
-                    echo form_input('email', set_value('email'), 'class="form-control form-control-lg"');
+                    echo form_input('email', set_value('email'), 'class="form-control form-control-lg" placeholder="email"');
                     echo form_label('Email', 'email');
                     ?>
                     <div class="error-message-style">
-                        <?php    
+                        <?php
                         echo form_error('email');
                         ?>
                     </div>
@@ -23,33 +23,53 @@
                     <!-- <input type="surname" class="form-control form-control-lg" placeholder="surname" />
                     <label for="surname">Nazwisko</label> -->
                     <?php
-                    echo form_input('firstname', set_value('firstname'), 'class="form-control form-control-lg"');
+                    echo form_input('firstname', set_value('firstname'), 'class="form-control form-control-lg" placeholder="imie"');
                     echo form_label('Imię', 'firstname');
                     ?>
+                    <div class="error-message-style">
+                        <?php
+                        echo form_error('firstname');
+                        ?>
+                    </div>
                 </div>
                 <div class="form-floating mb-4">
                     <!-- <input type="email" class="form-control form-control-lg" placeholder="email" />
                     <label for="email">Email</label> -->
                     <?php
-                    echo form_input('lastname', set_value('lastname'), 'class="form-control form-control-lg"');
+                    echo form_input('lastname', set_value('lastname'), 'class="form-control form-control-lg" placeholder="surname"');
                     echo form_label('Nazwisko', 'lastname');
                     ?>
+                    <div class="error-message-style">
+                        <?php
+                        echo form_error('lastname');
+                        ?>
+                    </div>
                 </div>
                 <div class="form-floating mb-3">
                     <!-- <input type="password" class="form-control form-control-lg" placeholder="password" />
                     <label for="password">Hasło</label> -->
                     <?php
-                    echo form_password('password', '', 'class="form-control form-control-lg"');
+                    echo form_password('password', '', 'class="form-control form-control-lg" placeholder="Hasło"');
                     echo form_label('Hasło', 'password');
                     ?>
+                    <div class="error-message-style">
+                        <?php
+                        echo form_error('password');
+                        ?>
+                    </div>
                 </div>
                 <div class="form-floating mb-3">
                     <!-- <input type="repeat" class="form-control form-control-lg" placeholder="repeat" />
                     <label for="repeat">Powtórz hasło</label> -->
                     <?php
-                    echo form_password('password2', '', 'class="form-control form-control-lg"');
+                    echo form_password('password2', '', 'class="form-control form-control-lg" placeholder="Powtórz hasło"');
                     echo form_label('Powtórz hasło', 'password2');
                     ?>
+                    <div class="error-message-style">
+                        <?php
+                        echo form_error('password2');
+                        ?>
+                    </div>
                 </div>
                 <div class="text-center mt-4">
                     <!-- <button type="button" class="btn btn-primary btn-lg single-button-style">Utwórz konto</button> -->
@@ -59,8 +79,8 @@
                     ?>
                 </div>
             </div>
-        <?php   
-        echo form_close();
-        ?>
+            <?php
+            echo form_close();
+            ?>
     </div>
 </div>
