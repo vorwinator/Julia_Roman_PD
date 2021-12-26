c34596c2ebaa882a91af132925dcf313cfb68a2a
vorwinator <80983036+vorwinator@users.noreply.github.com>
2021-12-26 14:47:57 +0100

    dodano kontenery na komunikaty błędów walidacji

--- application/views/Pages/authentication/login.php
+++ application/views/Pages/authentication/login.php
@@ -3,38 +3,48 @@
         <?php
         echo form_open('authentication/login', 'class="col-lg-9 pd-login"');
         ?>
-            <h1 class="big-header-style text-center">Zaloguj się</h1>
-            <div class="pd-top">
-                <div class="caption">
-                    <p class="small fw-bold mt-2 pt-1 mb-0">Nie posiadasz konta?<a href=<?php echo site_url('authentication/sign_up');?> class="link-primary"> Zarejestruj się</a></p>
-                </div>
-                <div class="form-floating mb-4">
+        <h1 class="big-header-style text-center">Zaloguj się</h1>
+        <div class="pd-top">
+            <div class="caption">
+                <p class="small fw-bold mt-2 pt-1 mb-0">Nie posiadasz konta?<a href=<?php echo site_url('authentication/sign_up'); ?> class="link-primary"> Zarejestruj się</a></p>
+            </div>
+            <div class="form-floating mb-4">
+                <?php
+                echo form_input('email', set_value('email'), 'class="form-control form-control-lg"');
+                echo form_label('Email', 'email');
+                ?>
+                <div class="error-message-style">
                     <?php
-                    echo form_input('email', set_value('email'), 'class="form-control form-control-lg"');
-                    echo form_label('Email', 'email');
+                    echo form_error('email');
                     ?>
                 </div>
-                <div class="form-floating mb-3">
+            </div>
+            <div class="form-floating mb-3">
+                <?php
+                echo form_password('password', '', 'class="form-control form-control-lg"');
+                echo form_label('Hasło', 'password');
+                ?>
+                <div class="error-message-style">
                     <?php
-                    echo form_password('password', '', 'class="form-control form-control-lg"');
-                    echo form_label('Hasło', 'password');
+                    echo form_error('password');
                     ?>
                 </div>
-                <!-- <div class="d-flex justify-content-between align-items-center">
+            </div>
+            <!-- <div class="d-flex justify-content-between align-items-center">
                     <div class="form-check mb-0 pd-right">
                         <input class="form-check-input me-2" type="checkbox" value="" id="logform" />
                         <label class="form-check-label" for="logform">Zapamiętaj mnie</label>
                     </div>
                     <a href="#" class="text-body">Przypomnij hasło</a>
                 </div> -->
-                <div class="text-center mt-4">
-                    <?php
-                    echo form_input('acc_type', 1, 'style="display:none;"');
+            <div class="text-center mt-4">
+                <?php
+                echo form_input('acc_type', 1, 'style="display:none;"');
 
-                    echo form_submit('submit', "Zaloguj", 'class="btn btn-primary btn-lg single-button-style"');
-                    ?>
-                </div>
+                echo form_submit('submit', "Zaloguj", 'class="btn btn-primary btn-lg single-button-style"');
+                ?>
             </div>
+        </div>
         <?php
         echo form_close();
         ?>
