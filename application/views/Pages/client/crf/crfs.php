b20b21fcac92c99c604d7bf757cd0475d0083ec5
vorwinator <vorwi111@gmail.com>
2022-01-03 16:48:11 +0100

    lista placówek

---
+++ application/views/Pages/client/crf/crfs.php
@@ -0,0 +1,23 @@
+<div class="container-fluid">
+    <div class="col-md-8 col-lg-6 col-xl-4 offset-1 mx-auto">
+        <h1 class="big-header-style text-center">Lista placówek - klient</h1>
+    </div>
+    <div class="row pd-space">
+        <?php
+        foreach($crfs as $crf){
+            $address_explode = explode(" ", $crf['address']);
+            if($crf['type']=="Oba")$crf_purpose="Zwroty i odbiory";
+            if($crf['type']=="Zwrot")$crf_purpose="Zwroty";
+            if($crf['type']=="Odbiór")$crf_purpose="Odbiory";
+        ?>
+        <div class="col-sm-4 placowka d-flex justify-content-center placowka">
+            <?php
+            echo $address_explode[1] ." ". $address_explode[2] ."<br>".
+            $address_explode[3] ." ". $address_explode[0] ."<br>
+            tel: 000 000 000<br>
+            email: car@net.com";
+            ?>
+        </div>
+        <?php } ?>
+    </div>
+</div>
