9aeee137aae749578416743ee3ad28ed2575623f
vorwinator <vorwi111@gmail.com>
2022-01-10 14:39:58 +0100

    routing dla rent.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -59,5 +59,6 @@ $route['car_rental_facility/(:any)'] = 'car_rental_facility/$1';
 $route['acc/(:any)'] = 'acc/$1';
 $route['car_all/(:any)'] = 'car_all/$1';
 $route['rental/(:any)'] = 'rental/$1';
+$route['rent/(:any)'] = 'rent/$1';
 $route['crf/(:any)'] = 'crf/$1';
 
