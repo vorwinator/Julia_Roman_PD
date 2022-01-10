b9e5a16dfb699be21d123b748cf4adc3571e3f6e
vorwinator <vorwi111@gmail.com>
2022-01-10 17:39:46 +0100

    przerobienie strony profilu klienta

--- application/views/Pages/client/acc/profile.php
+++ application/views/Pages/client/acc/profile.php
@@ -1,51 +1,61 @@
-<div class="container-fluid">
-    <div class="container-fluid padding-custom text-colour d-flex justify-content-center">
-        <h1 class="header-colour">Profil</h1>
-    </div>
-    <div class="container rounded mb-5">
-        <div class="row">
-            <div class="col-md-3 border-right">
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold"><?php echo $acc['firstname'] . " " . $acc['lastname'] ?></span><span class="text-black-50"><?php echo $acc['email'] ?></span><span> </span></div>
-            </div>
-            <div class="col-md-6 border-right">
-                <div class="p-3 py-5">
-                    <div class="row mt-2">
-                        <div class="col-md-6">
-                            <label class="labels">Imie</label>
-                            <?php
-                            echo form_open(current_url());
-
-                            echo form_input('firstname', $acc['firstname'], "placeholder=$acc[firstname] class=form-control");
-                            echo form_error('firstname');
-                            ?>
-                        </div>
-                        <div class="col-md-6">
-                            <label class="labels">Nazwisko</label>
-                            <?php
-                            echo form_input('lastname', $acc['lastname'], "placeholder=$acc[lastname] class=form-control");
-                            echo form_error('lastname');
-                            ?>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12">
-                            <label class="labels">Adres email</label>
-                            <?php
-                            echo form_input('email', $acc['email'], "placeholder=$acc[email] class=form-control");
-                            echo form_error('email');
-
-                            echo form_input('acc_type', 1, 'style="display:none;"');
-                            ?>
-                        </div>
-                    </div>
-                    <div class="mt-5 text-center">
+<div class="container-fluid d-flex justify-content-center">
+    <div class="col-md-8 col-lg-6 col-xl-4">
+        <div class="container rounded mb-5">
+            <?php
+            echo form_open(current_url(), 'class="col-lg-9 profile-form"');
+            ?>
+            <h1 class="big-header-style text-center" style="padding-bottom: 10%;">Profil</h1>
+            <div class="row">
+                <div class="col col-border">
+                    <div class="form-floating mb-3">
                         <?php
-                        echo form_submit('submit', "Zapisz", "class='btn btn-primary' style='margin-right: 2%'");
-
+                        echo form_input('firstname', $acc['firstname'], "placeholder=$acc[firstname] class=form-control form-control-lg");
+                        echo form_label('Imie', 'firstname');
+                        echo form_error('firstname');
+                        ?>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <?php
+                        echo form_input('lastname', $acc['lastname'], "placeholder=$acc[lastname] class=form-control form-control-lg");
+                        echo form_label('Nazwisko', 'lastname');
+                        echo form_error('lastname');
+                        ?>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <?php
+                        echo form_input('email', $acc['email'], "placeholder=$acc[email] class=form-control form-control-lg");
+                        echo form_label('Email', 'email');
+                        echo form_error('email');
+                        ?>
+                    </div>
+                    <div class="text-center">
+                        <?php
+                        echo form_submit('submit', "Zapisz", 'class="btn btn-primary single-button-style"');
+                        ?>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-floating mb-3">
+                        <input type="password" class="form-control form-control-lg" placeholder="password" />
+                        <label for="flpassword">Obecne hasło</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="password2" class="form-control form-control-lg" placeholder="password2" />
+                        <label for="password2">Nowe hasło</label>
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input type="repeat" class="form-control form-control-lg" placeholder="repeat" />
+                        <label for="repeat">Powtórz nowe hasło</label>
+                    </div>
+                    <div class="text-center">
+                        <?php
+                        echo form_submit('submit_password', "Zapisz", 'class="btn btn-primary single-button-style"');
                         echo form_close();
                         ?>
-                        <a href=<?php echo site_url('client/rental/rentals'); ?>><button class="btn btn-primary" type="button">Lista wypożyczeń</button></a>
                     </div>
+                </div>
+                <div class="col-xs-12 text-center pd-top">
+                    <a class="btn btn-outline-primary single-button-style" role="button" href=<?php echo site_url('client/rental/rentals'); ?>>Lista wynajmów</a>
                 </div>
             </div>
         </div>
