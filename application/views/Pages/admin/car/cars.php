161a44a2b746690c1d65edb42ff81e845c57e7e3
vorwinator <vorwi111@gmail.com>
2022-01-12 19:57:33 +0100

    usunięcie zbędnego kodu

--- application/views/Pages/admin/car/cars.php
+++ application/views/Pages/admin/car/cars.php
@@ -3,7 +3,7 @@
         <h1 class="big-header-style text-center">Lista samochodów - admin</h1>
     </div>
     <div class="col-md-8 col-lg-12 text-center pd-bottom">
-        <a role="button" class="btn btn-outline-primary full-width-btn" href=<?php echo site_url('admin/car/create');?>>Dodaj samochód</a>
+        <a role="button" class="btn btn-outline-primary full-width-btn" href=<?php echo site_url('admin/car/create'); ?>>Dodaj samochód</a>
     </div>
     <table id="example" class="display">
         <thead>
@@ -20,67 +20,67 @@
             </tr>
         </thead>
         <tbody>
-        <?php
-        foreach ($cars as $car){
-        ?>
-            <tr>
-                <td>
-                    <?php
-                    echo $car['id_car']
-                    ?>
-                </td>
-                <td>
-                    <?php
-                    echo $car['brand']
-                    ?>
-                </td>
-                <td>
-                    <?php
-                    echo $car['model']
-                    ?>
-                </td>
-                <td>
-                    <?php
-                    echo $car['release_year']
-                    ?>
-                </td>
-                <td>
-                    <?php
-                    echo $car['price_per_day']
-                    ?>
-                </td>
-                <td>
-                    <?php
-                    echo $car['mileage']
-                    ?>
-                </td>
-                <td>
-                    <?php
-                    if($car['rental_status']==0)echo "Nie";
-                    elseif($car['rental_status']==1)echo "Tak";
-                    ?>
-                </td>
-                <td style="width: 10%"><img class="img-fluid" style="max-width:50%" src="bil.png"></td>
-                <td>
-                    <!-- <button type="button" class="btn btn-primary btn-custom">Edytuj</button>
-                    <button type="button" class="btn btn-danger btn-custom">Usuń</button> -->
-                    <a href=<?php echo site_url('admin/car/update?id_car_update='.$car['id_car']);?>><button type="button" class="btn btn-primary btn-custom">Edytuj</button></a>
-                    <a href=<?php echo site_url('admin/car/delete?id_car_delete='.$car['id_car']);?> onclick="return confirm('Na pewno usunąć ten samochód?')"><button type="button" class="btn btn-danger btn-custom">Usuń</button></a>
-                </td>
-            </tr>
-        <?php } ?>
+            <?php
+            foreach ($cars as $car) {
+            ?>
+                <tr>
+                    <td>
+                        <?php
+                        echo $car['id_car']
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        echo $car['brand']
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        echo $car['model']
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        echo $car['release_year']
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        echo $car['price_per_day']
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        echo $car['mileage']
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        if ($car['rental_status'] == 0) echo "Nie";
+                        elseif ($car['rental_status'] == 1) echo "Tak";
+                        ?>
+                    </td>
+                    <td style="width: 10%">
+                        <img class="img-fluid" style="max-width:50%" src="bil.png" alt="Image not found" onerror="this.onerror=null;this.src=' <?php echo base_url(); ?>/assets/check.jpg';" />
+                    </td>
+                    <td>
+                        <a href=<?php echo site_url('admin/car/update?id_car_update=' . $car['id_car']); ?>><button type="button" class="btn btn-primary btn-custom">Edytuj</button></a>
+                        <a href=<?php echo site_url('admin/car/delete?id_car_delete=' . $car['id_car']); ?> onclick="return confirm('Na pewno usunąć ten samochód?')"><button type="button" class="btn btn-danger btn-custom">Usuń</button></a>
+                    </td>
+                </tr>
+            <?php } ?>
         </tbody>
         <tfoot>
             <tr>
-              <th>Id</th>
-              <th>Marka</th>
-              <th>Model</th>
-              <th>Rok produkcji</th>
-              <th>Cena za dobę</th>
-              <th>Przebieg w km</th>
-              <th>Czy wypożyczony</th>
-              <th>Zdjęcie</th>
-              <th></th>
+                <th>Id</th>
+                <th>Marka</th>
+                <th>Model</th>
+                <th>Rok produkcji</th>
+                <th>Cena za dobę</th>
+                <th>Przebieg w km</th>
+                <th>Czy wypożyczony</th>
+                <th>Zdjęcie</th>
+                <th></th>
             </tr>
         </tfoot>
     </table>
