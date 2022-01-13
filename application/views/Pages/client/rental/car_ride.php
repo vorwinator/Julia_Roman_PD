8ee379569d0f73688d7960e6531863a6cf6b19b6
vorwinator <vorwi111@gmail.com>
2022-01-13 17:33:18 +0100

    nowy formularz dla przejazdu z kierowcą

--- application/views/Pages/client/rental/car_ride.php
+++ application/views/Pages/client/rental/car_ride.php
@@ -59,46 +59,120 @@
         </div>
         <div class="row">
             <div class="etykieta">Wybierz termin</div>
-            <div class="col-md-6">
+            <div class="col-md-7">
                 <div class="form-floating mb-3">
-                    <input name="start_date" id="start_date" type="date" class="form-control form-control-lg" />
-                    <label for="start_date">Data rozpoczęcia</label>
+                    <input name="date" id="date" type="date" class="form-control form-control-lg" placeholder="Dzień przejazdu" />
+                    <label for="date">Dzień przejazdu</label>
                 </div>
                 <div class="error-message-style">
                     <?php
-                    echo form_error('start_date');
+                    echo form_error('date');
                     ?>
                     <?php
-                    if (isset($start_date_error)) echo $start_date_error;
+                    if (isset($date_error)) echo $date_error;
                     ?>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-md-5">
                 <div class="form-floating mb-3">
-                    <input name="end_date" id="end_date" type="date" class="form-control form-control-lg" />
-                    <label for="end_date">Data zakończenia</label>
+                    <input name="date2" id="date2" type="time" class="form-control form-control-lg" placeholder="Godzina przybycia" />
+                    <label for="date2">Godzina przybycia</label>
                 </div>
                 <div class="error-message-style">
                     <?php
-                    echo form_error('end_date');
+                    echo form_error('date2');
+                    ?>
+                    <?php
+                    if (isset($date2_error)) echo $date2_error;
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="etykieta">Wybierz adres startowy</div>
+            <div class="col-md-12">
+                <div class="form-floating mb-3">
+                    <?php
+                    echo form_input('start_address_street', set_value('start_address_street'), 'class="form-control form-control-lg" placeholder="Ulica"');
+                    echo form_label('Ulica', 'start_address_street');
+                    ?>
+                </div>
+                <div class="error-message-style">
+                    <?php
+                    echo form_error('start_address_street');
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="form-floating mb-3">
+                    <?php
+                    echo form_input('start_address_city', set_value('start_address_city'), 'class="form-control form-control-lg" placeholder="Miasto"');
+                    echo form_label('Miasto', 'start_address_city');
+                    ?>
+                </div>
+                <div class="error-message-style">
+                    <?php
+                    echo form_error('start_address_city');
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-floating mb-3">
+                    <?php
+                    echo form_input('start_address_postalcode', set_value('start_address_postalcode'), 'class="form-control form-control-lg" placeholder="Kod pocztowy"');
+                    echo form_label('Kod pocztowy', 'start_address_postalcode');
+                    ?>
+                </div>
+                <div class="error-message-style">
+                    <?php
+                    echo form_error('start_address_postalcode');
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="etykieta">Wybierz adres docelowy</div>
+            <div class="col-md-12">
+                <div class="form-floating mb-3">
+                    <?php
+                    echo form_input('finish_address_street', set_value('finish_address_street'), 'class="form-control form-control-lg" placeholder="Ulica"');
+                    echo form_label('Ulica', 'finish_address_street');
+                    ?>
+                </div>
+                <div class="error-message-style">
+                    <?php
+                    echo form_error('finish_address_street');
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="form-floating mb-3">
+                    <?php
+                    echo form_input('finish_address_city', set_value('finish_address_city'), 'class="form-control form-control-lg" placeholder="Miasto"');
+                    echo form_label('Miasto', 'finish_address_city');
+                    ?>
+                </div>
+                <div class="error-message-style">
+                    <?php
+                    echo form_error('finish_address_city');
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-floating mb-3">
+                    <?php
+                    echo form_input('finish_address_postalcode', set_value('finish_address_postalcode'), 'class="form-control form-control-lg" placeholder="Kod pocztowy"');
+                    echo form_label('Kod pocztowy', 'finish_address_postalcode');
+                    ?>
+                </div>
+                <div class="error-message-style">
+                    <?php
+                    echo form_error('finish_address_postalcode');
                     ?>
                 </div>
             </div>
         </div>
         <div class="col-md-12">
-            <div class="error-message-style">
-                <?php
-                if (isset($date_error)) echo $date_error;
-                ?>
-            </div>
-            <div class="etykieta">Wybierz placówkę odbioru</div>
-            <?php
-            echo form_dropdown('crf_address', $address_options, '', 'class="col-md-12 form-control-lg"');
-            ?>
-            <div class="etykieta">Wybierz placówkę zwrotu</div>
-            <?php
-            echo form_dropdown('return_to', $return_to_options, '', 'class="col-md-12 form-control-lg"');
-            ?>
             <div class="etykieta">Zarezerwuj ilość kilometrów</div>
             <div class="form-floating mb-3">
                 <?php
