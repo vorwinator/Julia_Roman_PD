4d9b99b6b81a5935200b9f2ddd2ab8cd2b190ede
vorwinator <vorwi111@gmail.com>
2022-01-14 06:35:36 +0100

    Obsługa dla wynajmu przejazdu z kierowcą

--- application/controllers/client/Rental.php
+++ application/controllers/client/Rental.php
@@ -9,6 +9,7 @@
             $this->load->model('car_m');
             $this->load->model('crf_m');
             $this->load->model('rental_m');
+            $this->load->model('car_ride_m');
         }
 
 
@@ -76,7 +77,7 @@
 
         public function accept()
         {
-            if(!isset($_GET['accept'])){
+            if(!isset($_POST['accept'])){
                 if(isset($_GET['price'])){
                     $data['price'] = $_GET['price'];
                     $_SESSION['rent_type']="Wynajem";
@@ -88,16 +89,35 @@
                 $this->render_page($this->directory_path, "accept", $data);
             }
             else{
-                if ($this->rental_m->set($_SESSION['selected_car_id'], $_SESSION['login_id_acc'], $_SESSION['crf_address'], $_SESSION['start_date'], $_SESSION['end_date'], $_SESSION['rent_type'])) {
-                    unset($_SESSION['selected_car_id']);
-                    unset($_SESSION['start_date']);
-                    unset($_SESSION['end_date']);
-                    unset($_SESSION['crf_address']);
-                    unset($_SESSION['rent_type']);
-                    redirect('client/Rental/rentals?info=Pomyślnie dodano wynajem.');
-                } 
-                else 
-                {
+                if($_SESSION['rent_type']=="Wynajem"){
+                    if ($this->rental_m->set($_SESSION['selected_car_id'], $_SESSION['login_id_acc'], $_SESSION['crf_address'], $_SESSION['start_date'], $_SESSION['end_date'])) {
+                        unset($_SESSION['selected_car_id']);
+                        unset($_SESSION['start_date']);
+                        unset($_SESSION['end_date']);
+                        unset($_SESSION['crf_address']);
+                        unset($_SESSION['rent_type']);
+                        redirect('client/Rental/rentals?info=Pomyślnie dodano wynajem.');
+                    } 
+                    else 
+                    {
+                        $data['info'] = 'Niepowodzenie. Nie udało się zmodyfikować danych w bazie.';
+                        $this->render_page($this->directory_path, "accept", $data);
+                    }
+                }
+                if ($_SESSION['rent_type'] == "Przejazd z kierowcą") {
+                    if ($this->car_ride_m->set($_SESSION['login_id_acc'] ,$_SESSION['selected_car_id'] ,$_SESSION['date'], $_SESSION['start_address'], $_SESSION['finish_address'], $_SESSION['kilometers'])) {
+                        unset($_SESSION['date']);
+                        unset($_SESSION['start_address']);
+                        unset($_SESSION['finish_address']);
+                        unset($_SESSION['rent_type']);
+                        unset($_SESSION['kilometers']);
+                        redirect('client/Rental/rentals?info=Pomyślnie dodano Przejazd.');
+                    } else {
+                        $data['info'] = 'Niepowodzenie. Nie udało się zmodyfikować danych w bazie.';
+                        $this->render_page($this->directory_path, "accept", $data);
+                    }
+                }
+                else{
                     $data['info'] = 'Niepowodzenie. Nie udało się zmodyfikować danych w bazie.';
                     $this->render_page($this->directory_path, "accept", $data);
                 }
@@ -117,49 +137,46 @@
             if (isset($_GET['info'])) $data['info'] = $_GET['info'];
             if (isset($_GET['id_car_details'])) $_SESSION['selected_car_id'] = $_GET['id_car_details'];
             $data['car'] = $this->car_m->get_car_by_id($_SESSION['selected_car_id']);
-            $data['address_options'] = $this->crf_m->get_crfs_addresses();
-            $data['return_to_options'] = $this->crf_m->get_return_to_addresses();
-            $start_date = null;
-            $end_date = null;
 
-            $this->form_validation->set_rules('start_date', 'Datę rozpoczęcia', 'required');
-            $this->form_validation->set_rules('end_date', 'Datę zakończenia', 'required');
+            $this->form_validation->set_rules('start_address_street', 'Ulicę', 'required');
+            $this->form_validation->set_rules('start_address_city', 'Miasto', 'required');
+            $this->form_validation->set_rules('start_address_postalcode', 'Kod pocztowy', 'required');
+            $this->form_validation->set_rules('finish_address_street', 'Ulicę', 'required');
+            $this->form_validation->set_rules('finish_address_city', 'Miasto', 'required');
+            $this->form_validation->set_rules('finish_address_postalcode', 'Kod pocztowy', 'required');
+            $this->form_validation->set_rules('date', 'Dzień', 'required');
+            $this->form_validation->set_rules('date2', 'Godzinę', 'required');
             $this->form_validation->set_rules('kilometers', 'Kilometry', 'required|integer');
 
             if ($this->form_validation->run() === TRUE) {
                 $validation = true;
-                if (isset($_POST['start_date'])) {
-                    $start_date = $_POST['start_date'];
-                    $start = strtotime($_POST['start_date']);
-                }
-                if (isset($_POST['end_date'])) {
-                    $end_date = $_POST['end_date'];
-                    $end = strtotime($_POST['end_date']);
-                }
-                if (isset($_POST['start_date']) && isset($_POST['end_date'])) {
-                    $start_end = $start - $end;
-                }
 
                 if (isset($_POST['submit'])) {
-                    if ($start_end > 0) {
-                        $validation = false;
-                        $data['date_error'] = "Data rozpoczęcia musi być wcześniejsza niż data zakończenia.";
-                    } else if ($start_date < date("Y-m-d")) {
-                        $validation = false;
-                        $data['start_date_error'] = "Data rozpoczęcia nie może mieć miejsca w przeszłości.";
+                    if ($_POST['date'] == date("Y-m-d")){
+                        if ($_POST['date2'] < date("H:i")) {
+                            $validation = false;
+                            $data['date2_error'] = "Godzina rozpoczęcia nie może mieć miejsca w przeszłości.";
+                        }
                     }
-                } else {
+                    
+                    if ($_POST['date'] < date("Y-m-d")) {
+                        $validation = false;
+                        $data['date_error'] = "Data rozpoczęcia nie może mieć miejsca w przeszłości.";
+                    }
+                } 
+                else {
                     $validation = false;
                 }
 
 
                 if ($validation === FALSE) {
                     $this->render_page($this->directory_path, "car_ride", $data);
-                } else {
-                    $_SESSION['crf_address'] = $_POST['crf_address'];
-                    $_SESSION['start_date'] = $start_date;
-                    $_SESSION['end_date'] = $end_date;
-                    $_SESSION['return_to'] = $_POST['return_to'];
+                } 
+                else {
+                    $_SESSION['date'] = $_POST['date'] . " " . $_POST['date2'];
+                    $_SESSION['start_address'] = $_POST['start_address_street'] . " " . $_POST['start_address_city'] . " " . $_POST['start_address_postalcode'];
+                    $_SESSION['finish_address'] = $_POST['finish_address_street'] . " " . $_POST['finish_address_city'] . " " . $_POST['finish_address_postalcode'];
+                    $_SESSION['kilometers'] = $_POST['kilometers'];
                     $price = $_POST["kilometers"] * $data["car"]["price_per_kilometer_with_chauffeur"];
                     redirect('client/Rental/accept?price_for_kilometers=' . $price);
                 }
