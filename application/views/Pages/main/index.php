ef9aca29b8c06726b5bc9a4fda558be608a2b6d6
macrometasomakosmos <46371003+macrometasomakosmos@users.noreply.github.com>
2022-01-15 12:23:26 +0100

    strona glowna

--- application/views/Pages/main/index.php
+++ application/views/Pages/main/index.php
@@ -1,2 +1,44 @@
-domek
-<?php echo "to jest domek" ?>
+<div class="container-fluid d-flex justify-content-center">
+    <div class="col-md-8 col-lg-6 col-xl-4 offset-1">
+        <h1 class="big-header-style text-center">Wypożyczalnia samochodów</h1>
+    </div>
+</div>
+<div class="desc">Od ponad 20 lat zajmujemy się wypożyczaniem samochodów na terenie całego kraju, aby ułatwić podróżującym transport i zapewnić szybkie oraz bezpieczne usługi.</p>
+<div id="carouselExampleIndicators" class="carousel carousel-abc slide" data-bs-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" style="object-fit: cover; width: 100%; height:500px" src="https://www.elleman.pl/media/cache/big/uploads/media/default/0005/39/80d30ad1deab06645d8f32642ff7fd3bc121b310.jpeg" alt="First slide">  
+              <div class="carousel-caption d-none d-md-block">
+                  <h5>Nasza oferta</h5>
+                  <p>Posiadamy ponad 1700 aut osobowych, ciężarowych i innych</p>
+              </div>
+          </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" style="object-fit: cover; width: 100%; height:500px" src="https://motopedia.otomoto.pl/wp-content/uploads/2021/01/z24236394IHAudi-e-tron-GT.jpeg" alt="Second slide">
+              <div class="carousel-caption d-none d-md-block">
+                  <h5>Odbierz, gdzie chcesz</h5>
+                  <p>Placówki odbioru znajdują się w całym kraju</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" style="object-fit: cover; width: 100%; height:500px" src="https://villalittleparadise.com/wp-content/uploads/2021/05/Car-Rental.png" alt="Third slide">
+              <div class="carousel-caption d-none d-md-block">
+                  <h5>Usługi szoferskie</h5>
+                  <p>Oferujemy także możliwość wynajęcia kierowcy</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
