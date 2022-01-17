eb83d573a44053143607e3d46faa9d918a3b1e76
vorwinator <vorwi111@gmail.com>
2022-01-17 17:40:57 +0100

    przywracanie statusu samochodom które powróciły z wynajmu i przejazdów

--- application/controllers/Car_all.php
+++ application/controllers/Car_all.php
@@ -7,14 +7,29 @@
         {
             parent::__construct();
             $this->load->model('car_m');
+            $this->load->model('rental_m');
+            $this->load->model('car_ride_m');
         }
 
 
         public function cars()
         {
+            $rentals = $this->rental_m->get_rentals();
+            if($rentals!=null) foreach($rentals as $rental){
+                if($rental['end_date'] > date("Y-m-d")){
+                    $this->rental_m->rental_status_off($rental['id_car']);
+                }
+            }
+
+            $car_rides = $this->rental_m->get_rentals();
+            if($car_rides!=null) foreach($car_rides as $car_ride){
+                if($car_ride['date'] > date("Y-m-d")){
+                    $this->car_ride_m->rental_status_off($car_ride['id_car']);
+                }
+            }
+
             $data['title'] = "Lista samochodów do wynajęcia";
             if(isset($_GET['info']))$data['info']=$_GET['info'];
-            $data['welcome'] = "To jest lista aut";
             $data['cars'] = $this->car_m->get_unique_cars_with_details_free_to_rent();
             
             $this->render_page($this->directory_path, "cars", $data);
